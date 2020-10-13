e0ea0989eb9fb82451f469cd71c67e351996321c
Mattan1999 <mattias470@gmail.com>
2020-10-14 00:05:17 +0200

    Updated game functionality
    + Added a WindowManager
    + Added code that hides the word a user should guess
    + Added code to the onGuessClick method
    + Started to work on a revealWordToGuess method that reveals a letter if it exists in the word to guess

--- app/src/main/java/com/example/hangman/GameActivity.java
+++ app/src/main/java/com/example/hangman/GameActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,7 +43,7 @@ public class GameActivity extends AppCompatActivity {
     // Max errors before user lose
     public static final int MAX_ERRORS = 6;
     // Word to reveal
-    private char[] wordToGuess;
+    private String wordToGuess;
     // Word to reveal stored in a char array to show progression of user
     private char[] lettersInWordFound;
     private int userErrors;
@@ -61,7 +65,7 @@ public class GameActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Hang Man");
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
-
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         initializeGame();
         startGame();
     }
@@ -88,25 +92,48 @@ public class GameActivity extends AppCompatActivity {
         img = findViewById(R.id.img);
         remainingTries = findViewById(R.id.remaining_tries);
         wordToGuessTv = findViewById(R.id.word_to_find);
+        userInput = findViewById(R.id.user_input);
+        wrongGuesses = findViewById(R.id.wrong_guesses);
         wordToGuess = hiddenWordToFind();
+        lettersInWordFound = new char[wordToGuess.length()];
+
+        for (int i = 0; i < lettersInWordFound.length; i++){
+            lettersInWordFound[i] = '_';
+        }
     }
 
     public void startGame(){
         userErrors = -1;
         letters.clear();
-
-        lettersInWordFound = new char[wordToGuess.length];
-
-        for (int i = 0; i < lettersInWordFound.length; i++){
-            lettersInWordFound[i] = '_';
-        }
         updateImg(userErrors);
-        wordToGuessTv.setText();
+        wordToGuessTv.setText(printWordToGuess(lettersInWordFound));
     }
 
     // Method returning randomly next word to find
-    private char[] hiddenWordToFind() {
-        return myListOfWords[random.nextInt(myListOfWords.length)].toCharArray();
+    private String hiddenWordToFind() {
+        return myListOfWords[random.nextInt(myListOfWords.length)];
+    }
+
+    private String printWordToGuess(char[] array) {
+        StringBuilder hiddenWord = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            hiddenWord.append(array[i]);
+            if (i < array.length - 1) {
+                hiddenWord.append(" ");
+            }
+        }
+        return hiddenWord.toString();
+    }
+
+    private void revealWordToGuess(String letter) {
+        int indexOfLetter = wordToGuess.indexOf(letter);
+
+        while (indexOfLetter >= 0) {
+            lettersInWordFound[indexOfLetter] = wordToGuess.charAt(indexOfLetter);
+            indexOfLetter = wordToGuess.indexOf(letter, indexOfLetter + 2);
+        }
+
+        wordToGuessTv.setText(printWordToGuess(lettersInWordFound));
     }
 
     private void updateImg(int play) {
@@ -116,12 +143,35 @@ public class GameActivity extends AppCompatActivity {
     }
 
     public void onGuessClick(View view){
+        String input = userInput.getText().toString();
         if (userErrors < MAX_ERRORS) {
-            userInput.setText("");
-            updateImg(userErrors);
+            try {
+                if (input.length() < 2) {
+                    if (!letters.contains(input)) {
+                        if (wordToGuess.contains(input)) {
+                            revealWordToGuess(input);
+                            userInput.setText("");
+                        } else {
+                            userErrors++;
+                            userInput.setText("");
+                        }
+                        letters.add(input);
+                        wrongGuesses.setText(letters.toString());
+                    } else {
+                        Toast.makeText(this, R.string.character_already_exist, Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                    wordToGuessTv.setText(printWordToGuess(lettersInWordFound));
+                    updateImg(userErrors);
+                } else {
+                    Toast.makeText(this, R.string.too_many_characters, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, R.string.no_characters, Toast.LENGTH_SHORT)
+                        .show();
+            }
         }
     }
-
-
 
 }
